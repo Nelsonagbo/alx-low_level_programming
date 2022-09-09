de54e00c97876704adebfcb68452495596c5b3bd
Nelson Agbo <nelsonagbo@gmail.com>
2022-09-08 18:28:56 -0700

    Programming is like a building

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the put function
+ * main - printing
  * 
- * Description: using the main function
- * this program prints "programming is like building a multilingual puzzle
- * Return: 0
+ * Return: Always 0 (Successful)
  */
 int main(void)
 {
-	printf("with proper grammar, but the oytcome is a piece of art,\n");
-	return (0)
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
