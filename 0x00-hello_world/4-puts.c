204d8dbb089a488a2f349263a5d3e17dde2c7e40
Nelson Agbo <nelsonagbo@gmail.com>
2022-09-08 17:24:48 -0700

    Programming is like a building

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,17 +3,12 @@
 
 
 /**
- *
- * * main - Printing
- *
- * *
- *
- * * Return: Always 0 (Successful)
- *
- * */
+ * main - Printing
+ * 
+ * Return: Always 0 (Successful)
+ */
 
 int main(void)
-
 {
 
 	puts("\"Programming is like building a multilingual
