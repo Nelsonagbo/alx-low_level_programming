3fceb1b2dd9626fe7123322cd263cd351d2ca4e3
Nelson Agbo <nelsonagbo@gmail.com>
2022-09-11 14:11:16 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints all combinations of single digit numbers
+ * Description: using the main function
+ * thsi program prints "combinations of single digit numbers"
+ * Return: 0
+ */
+int main(void)
+{
+	int b;
+
+	for (b = 48; b <= 57; c++)
+	{
+		putchar(b);
+		if (b != 57)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}	
