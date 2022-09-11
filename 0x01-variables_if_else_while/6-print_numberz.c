924feb9f002bb77f1ddfb9e1165c71c7927b9183
Nelson Agbo <nelsonagbo@gmail.com>
2022-09-11 11:51:07 -0700

    print numbers for 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - prints single digit base numbers from 0
+ * Description: using the main function
+ * this program prints "Programming is singke digit number starting from 0"
+ * Return: 0
+ */int main(void)
+{
+	int n;
+
+	for (n = '0'; n <= '9'; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0)
+}
